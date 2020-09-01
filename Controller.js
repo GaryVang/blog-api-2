f2c3ae3cfaf7c22dcd61f6cba8d5379767997ca7
MysticV <leng_vang89@hotmail.com>
2020-09-01 05:00:31 -0700

    Added a default initial pull request and an auth path for initial session check.

--- Controller.js
+++ Controller.js
@@ -1,185 +1,250 @@
-//Add a layer of validation
-const registerUser = (db, bcrypt, saltRounds) => (req, res) => {
-// const registerUser = (db, req, res, bcrypt, saltRounds)  => {
+const getInitial = (db) => async (req, res) => {
+  let postCount;
+  let postList;
+  const OFFSET = 5 * (req.params.page - 1) || 0;
 
-    const USERNAME = req.body.username;
-    const PASSWORD_TEXT = req.body.password;
-
-    db.many('SELECT username FROM users WHERE username=$1', [USERNAME])
-        .then(() => {
-            console.log('Failure: User Already Exists');
-            res.status(200).json({status: false, comment:'User Already Exists'});
-        })
-    .catch(error => {
-        // error;
-        bcrypt.hash(PASSWORD_TEXT, saltRounds, function(err, hash) {
-            db.none('INSERT INTO users(username, password) VALUES($1, $2)', [USERNAME, hash])
-                .then(() => {
-                    // success;
-                    // res.json('User Registration Successful');
-                    res.status(200).json({status: true, comment:'User Registration Successful'});
-                    console.log('Success: ', USERNAME, PASSWORD_TEXT, hash);
-                })
-            .catch(error => {
-                // error;
-                console.log('Error: User Registration Failed. ', error);
-                res.status(400).json({status: false, comment: 'SignIn Failed: Wrong Password'});
-            });
-        });
+  await db
+    .one("SELECT count(*)  FROM posts")
+    .then((result) => {
+      postCount = parseInt(result.count);
+      // res.status(200).json(parseInt(result.count));
+    })
+    .catch((error) => {
+      console.log("Error: ", error);
+      // res.status(400).json('Error: DB has no posts');
+      postCount = 0;
     });
 
-    // bcrypt.hash(PASSWORD_TEXT, saltRounds, function(err, hash) {
-    //     db.none('INSERT INTO users(username, password) VALUES($1, $2)', [USERNAME, hash])
-    //         .then(() => {
-    //             // success;
-    //             // res.json('User Registration Successful');
-    //             res.status(200).json({status: true, comment:'User Registration Successful'});
-    //             console.log('Success: ', USERNAME, PASSWORD_TEXT, hash);
-    //         })
-    //     .catch(error => {
-    //         // error;
-    //         console.log('Error: User Registration Failed. ', error);
-    //         res.status(400).json({status: false, comment: 'SignIn Failed: Wrong Password'});
-    //     });
-    // });
+  await db.many(
+    "SELECT username, post_id, title, body, to_char(post_date, $3) as post_date FROM posts INNER JOIN users ON posts.user_id=users.user_id ORDER BY post_date desc LIMIT 5 OFFSET $1",
+    [OFFSET, "second", "YYYY-MM-DD HH12:MI:SS AM (TZ)"]
+  )
+    .then((result) => {
+      postList = result;
+    //   res.status(200).json({ status: true, data: result });
+    })
+    .catch((error) => {
+      // error;
+      console.log("Error: ", error.result);
+      res.status(400).json({ status: false, comment: "Error: No blog posts retrieved" });
+    });
+
+    res.status(200).json({postList: postList, postCount: postCount});
+};
+
+//Add a layer of validation
+const registerUser = (db, bcrypt, saltRounds) => (req, res) => {
+  // const registerUser = (db, req, res, bcrypt, saltRounds)  => {
+
+  const USERNAME = req.body.username;
+  const PASSWORD_TEXT = req.body.password;
+
+  db.many("SELECT username FROM users WHERE username=$1", [USERNAME])
+    .then(() => {
+      console.log("Failure: User Already Exists");
+      res.status(200).json({ status: false, comment: "User Already Exists" });
+    })
+    .catch((error) => {
+      // error;
+      bcrypt.hash(PASSWORD_TEXT, saltRounds, function (err, hash) {
+        db.none("INSERT INTO users(username, password) VALUES($1, $2)", [
+          USERNAME,
+          hash,
+        ])
+          .then(() => {
+            // success;
+            // res.json('User Registration Successful');
+            res
+              .status(200)
+              .json({ status: true, comment: "User Registration Successful" });
+            console.log("Success: ", USERNAME, PASSWORD_TEXT, hash);
+          })
+          .catch((error) => {
+            // error;
+            console.log("Error: User Registration Failed. ", error);
+            res
+              .status(400)
+              .json({
+                status: false,
+                comment: "SignIn Failed: Wrong Password",
+              });
+          });
+      });
+    });
+
+  // bcrypt.hash(PASSWORD_TEXT, saltRounds, function(err, hash) {
+  //     db.none('INSERT INTO users(username, password) VALUES($1, $2)', [USERNAME, hash])
+  //         .then(() => {
+  //             // success;
+  //             // res.json('User Registration Successful');
+  //             res.status(200).json({status: true, comment:'User Registration Successful'});
+  //             console.log('Success: ', USERNAME, PASSWORD_TEXT, hash);
+  //         })
+  //     .catch(error => {
+  //         // error;
+  //         console.log('Error: User Registration Failed. ', error);
+  //         res.status(400).json({status: false, comment: 'SignIn Failed: Wrong Password'});
+  //     });
+  // });
 };
 
 //Add a layer of validation
 const signInUser = (db, bcrypt) => (req, res) => {
-    // req.session.dodo = 1;
-    // req.session.user = 'OPGG';
-    // console.log('outside: ', req.session);
-     if (req.session.views) {
-        req.session.views++;
-      }
-      else {
-        req.session.views = 1;
-      }
+  // req.session.dodo = 1;
+  // req.session.user = 'OPGG';
+  // console.log('outside: ', req.session);
+  if (req.session.views) {
+    req.session.views++;
+  } else {
+    req.session.views = 1;
+  }
 
-    // console.log('req: ', req.body);
-    const USERNAME = req.body.username;
-    const PASSWORD_TEXT = req.body.password;
+  // console.log('req: ', req.body);
+  const USERNAME = req.body.username;
+  const PASSWORD_TEXT = req.body.password;
 
-    db.one('SELECT user_id, password FROM users WHERE username=$1', [USERNAME])
-    .then(qResult => {
-        const PASSWORD_HASH = qResult.password;
+  db.one("SELECT user_id, password FROM users WHERE username=$1", [USERNAME])
+    .then((qResult) => {
+      const PASSWORD_HASH = qResult.password;
 
-        bcrypt.compare(PASSWORD_TEXT, PASSWORD_HASH, function(err, cResult) {
-            if(cResult){
-                // res.json('SignIn Successful', USERNAME);
-                // res.status(200).json(USERNAME);
-                // console.log('inside: ', req.session);
-                // req.session.user = 'bart1112';
-                req.session.user = USERNAME;
+      bcrypt.compare(PASSWORD_TEXT, PASSWORD_HASH, function (err, cResult) {
+        if (cResult) {
+          // res.json('SignIn Successful', USERNAME);
+          // res.status(200).json(USERNAME);
+          // console.log('inside: ', req.session);
+          // req.session.user = 'bart1112';
+          req.session.user = USERNAME;
 
-                res.status(200).json({status: true, user_id: qResult.user_id, username: USERNAME, comment: USERNAME + ' SignIn Successful'});
-            } else {
-                res.status(400).json({status: false, comment:'SignIn Failed: Wrong Password'});
-            }
-        });
+          res
+            .status(200)
+            .json({
+              status: true,
+              user_id: qResult.user_id,
+              username: USERNAME,
+              comment: USERNAME + " SignIn Successful",
+            });
+        } else {
+          res
+            .status(400)
+            .json({ status: false, comment: "SignIn Failed: Wrong Password" });
+        }
+      });
     })
-    .catch(error => {
-        // error;    
-        console.log('Error: ', error);
-        res.status(400).json({status: false, comment:'Error: User does not exist.'});
+    .catch((error) => {
+      // error;
+      console.log("Error: ", error);
+      res
+        .status(400)
+        .json({ status: false, comment: "Error: User does not exist." });
     });
 };
 
 const getPosts = (db) => (req, res) => {
-    console.log('User: ', req.session.user);
-    // const OFFSET = req.body.offset;
-    const OFFSET = (5*(req.params.page-1)) || 0;
-    // const PAGE = req.params.page - 1;
-    // console.log('page: ', req.params.page);
-    // console.log('offset :', OFFSET);
+  console.log("User: ", req.session.user);
+  // const OFFSET = req.body.offset;
+  const OFFSET = 5 * (req.params.page - 1) || 0;
+  // const PAGE = req.params.page - 1;
+  // console.log('page: ', req.params.page);
+  // console.log('offset :', OFFSET);
 
-    // let maxPosts;
-    // let postArr;
+  // let maxPosts;
+  // let postArr;
 
-    // await db.one('SELECT count(*) as max_post_count FROM posts')
-    // .then(result => {
-    //     console.log('max_post_count', result);
-    //     maxPosts = result;
-    // })
-    // .catch(error => {
-    //     // error;    
-    //     console.log('Error: ', error);
-    //     res.status(400).json('Error: DB has no posts');
-    // });
+  // await db.one('SELECT count(*) as max_post_count FROM posts')
+  // .then(result => {
+  //     console.log('max_post_count', result);
+  //     maxPosts = result;
+  // })
+  // .catch(error => {
+  //     // error;
+  //     console.log('Error: ', error);
+  //     res.status(400).json('Error: DB has no posts');
+  // });
 
-    db.many('SELECT username, post_id, title, body, to_char(post_date, $3) as post_date FROM posts INNER JOIN users ON posts.user_id=users.user_id ORDER BY post_date desc LIMIT 5 OFFSET $1', 
-        [OFFSET, 'second', 'YYYY-MM-DD HH12:MI:SS AM (TZ)'])
-    .then(result => {
-        // console.log(result);
-        // postArr = result;
-        res.status(200).json({status: true, data:result});
+  db.many(
+    "SELECT username, post_id, title, body, to_char(post_date, $3) as post_date FROM posts INNER JOIN users ON posts.user_id=users.user_id ORDER BY post_date desc LIMIT 5 OFFSET $1",
+    [OFFSET, "second", "YYYY-MM-DD HH12:MI:SS AM (TZ)"]
+  )
+    .then((result) => {
+      // console.log(result);
+      // postArr = result;
+      res.status(200).json({ status: true, data: result });
     })
-    .catch(error => {
-        // error;    
-        console.log('Error: ', error.result);
-        res.status(400).json({status: false, comment:'Error: No blog posts retrieved'});
+    .catch((error) => {
+      // error;
+      console.log("Error: ", error.result);
+      res
+        .status(400)
+        .json({ status: false, comment: "Error: No blog posts retrieved" });
     });
 
-    // res.status(200).json({maxPosts: maxPosts, postArr: postArr});
+  // res.status(200).json({maxPosts: maxPosts, postArr: postArr});
 };
 
 const submitPost = (db) => (req, res) => {
-    const TITLE = req.body.title;
-    const CONTENT = req.body.content;
-    const USER_ID = req.body.user_id;
+  const TITLE = req.body.title;
+  const CONTENT = req.body.content;
+  const USER_ID = req.body.user_id;
 
-    db.none('INSERT INTO posts(title, body, user_id) VALUES($1, $2, $3)', [TITLE, CONTENT, USER_ID])
-        .then(() => {
-            // success;
-            // res.json('Post Submition Successful');
-            res.status(200).json({status: true, comment: 'Post Submition Successful'});
-            console.log('Success: ', TITLE, CONTENT, USER_ID);
-        })
-    .catch(error => {
-        // error;
-        console.log('Error: Post Submition Failed ', error);
-        res.status(400).json({status: false, comment:'Post Submition Failed'});
+  db.none("INSERT INTO posts(title, body, user_id) VALUES($1, $2, $3)", [
+    TITLE,
+    CONTENT,
+    USER_ID,
+  ])
+    .then(() => {
+      // success;
+      // res.json('Post Submition Successful');
+      res
+        .status(200)
+        .json({ status: true, comment: "Post Submition Successful" });
+      console.log("Success: ", TITLE, CONTENT, USER_ID);
+    })
+    .catch((error) => {
+      // error;
+      console.log("Error: Post Submition Failed ", error);
+      res.status(400).json({ status: false, comment: "Post Submition Failed" });
     });
 
-    // db.none('INSERT INTO users(username, password) VALUES($1, $2)', [USERNAME, hash])
-    //         .then(() => {
-    //             // success;
-    //             res.json('User Registration Successful');
-    //             console.log('Success: ', USERNAME, PASSWORD_TEXT, hash);
-    //         })
-    // .catch(error => {
-    //     // error;
-    //     console.log('Error: User Registration Failed. ', error);
-    //     res.status(400).json('SignIn Failed: Wrong Password');
-    // });
+  // db.none('INSERT INTO users(username, password) VALUES($1, $2)', [USERNAME, hash])
+  //         .then(() => {
+  //             // success;
+  //             res.json('User Registration Successful');
+  //             console.log('Success: ', USERNAME, PASSWORD_TEXT, hash);
+  //         })
+  // .catch(error => {
+  //     // error;
+  //     console.log('Error: User Registration Failed. ', error);
+  //     res.status(400).json('SignIn Failed: Wrong Password');
+  // });
 };
 
 const getMaxPostCount = (db) => (req, res) => {
-//  if (req.session.views) {
-//         req.session.views++;
-//       }
-//       else {
-//         req.session.views = 1;
-//       }
-//       res.send(`${req.session.views} views`);
+  //  if (req.session.views) {
+  //         req.session.views++;
+  //       }
+  //       else {
+  //         req.session.views = 1;
+  //       }
+  //       res.send(`${req.session.views} views`);
 
-    db.one('SELECT count(*)  FROM posts')
-    .then(result => {
-        // console.log('max_post_count', parseInt(result.count));
-        // maxPosts = result;
-        res.status(200).json(parseInt(result.count));
+  db.one("SELECT count(*)  FROM posts")
+    .then((result) => {
+      // console.log('max_post_count', parseInt(result.count));
+      // maxPosts = result;
+      res.status(200).json(parseInt(result.count));
     })
-    .catch(error => {
-        // error;    
-        console.log('Error: ', error);
-        res.status(400).json('Error: DB has no posts');
+    .catch((error) => {
+      // error;
+      console.log("Error: ", error);
+      res.status(400).json("Error: DB has no posts");
     });
 };
 
 module.exports = {
-    registerUser,
-    signInUser,
-    getPosts,
-    submitPost,
-    getMaxPostCount
+  getInitial,
+  registerUser,
+  signInUser,
+  getPosts,
+  submitPost,
+  getMaxPostCount,
 };
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const pgp = require('pg-promise')();
+const express = require("express");
+const bodyParser = require("body-parser");
+const pgp = require("pg-promise")();
 
-const cors = require('cors');
-const bcrypt = require('bcrypt');
+const cors = require("cors");
+const bcrypt = require("bcrypt");
 
-const session = require('express-session');
-const pgSession = require('connect-pg-simple')(session);
+const session = require("express-session");
+const pgSession = require("connect-pg-simple")(session);
 
 const saltRounds = 10;
 
-const Controller = require('./Controller');
+const Controller = require("./Controller");
 
-const db = pgp('postgres://postgres:test123@localhost:5432/blog');
+const db = pgp("postgres://postgres:test123@localhost:5432/blog");
 
 const app = express();
 
@@ -21,7 +21,7 @@ const app = express();
 // app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // app.use(cors());
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 // app.set('trust proxy', 1)
 
 // app.use(function(req, res, next) {
@@ -35,113 +35,113 @@ app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 // });
 
 app.use(
-    session({
-        store: new pgSession({
-            pgPromise: db
-            // tableName: 'user_sessions'
-        }),
-        // name: '_somecookie',
-        key: 'user_sid',
-        secret: "catdog",
-        saveUninitialized: false,
-        resave: false,
-        cookie: {
-            secure: false,
-            // sameSite: 'none',
-            httpOnly: false,
-            // maxAge: 60000
-            maxAge: 5 * 60 * 1000 // 5 minutes: min * second * millisecond
-        }
-    })
+  session({
+    store: new pgSession({
+      pgPromise: db,
+      // tableName: 'user_sessions'
+    }),
+    // name: '_somecookie',
+    key: "user_sid",
+    secret: "catdog",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      secure: false,
+      // sameSite: 'none',
+      httpOnly: false,
+      // maxAge: 60000
+      maxAge: 5 * 60 * 1000, // 5 minutes: min * second * millisecond
+    },
+  })
 );
 
 app.use((req, res, next) => {
-    console.log('user: ', req.session.user);
-    console.log('session: ', req.sessionID);
+  console.log("user: ", req.session.user);
+  console.log("session: ", req.sessionID);
 
-    next();
+  next();
 });
 
 //powershell
 //$env:PORT="3005" ; node server.js
 const PORT = process.env.PORT || 3005;
-const NODE_ENV = process.env.NODE_ENV || 'development';
+const NODE_ENV = process.env.NODE_ENV || "development";
 
 const DB_PASSWORD = process.env["DB_PASSWORD"];
 
-
 const sessionChecker = (req, res, next) => {
-    if (req.session.user && req.headers.cookie.includes('user_sid')) {
+  if (req.session.user && req.headers.cookie.includes("user_sid")) {
     // if (req.session.user && req.session.cookie.user_sid) {
     // if (req.session.user) {
-        // res.redirect('/dashboard');
-        console.log('today: ', req.session.user);
-        // res.status(200).json({username: req.session.user});
-        next();
-    } else {
-        // console.log('req.session.cookie: ',  req.headers.cookie.split('='));
-        // console.log('req.session.cookie: ',  req.headers.cookie.includes('user_sid'));
-        next();
-    }    
+    // res.redirect('/dashboard');
+    console.log("today: ", req.session.user);
+    // res.status(200).json({username: req.session.user});
+    next();
+  } else {
+    // console.log('req.session.cookie: ',  req.headers.cookie.split('='));
+    // console.log('req.session.cookie: ',  req.headers.cookie.includes('user_sid'));
+    next();
+  }
 };
 
-app.get('/', sessionChecker, (req, res) => {
-    // res.json('Hello World');
-    if (req.session.user) {
-        // res.redirect('/dashboard');
-        console.log('today: ', req.session.user);
-        res.status(200).json({username: req.session.user});
-    } else {
-        // console.log(555555);
-        // res.clearCookie('user_sid');
-        // next();
-        res.status(400).json("No Valid Login");
-    }   
+app.get("/", Controller.getInitial(db));
+// app.get("/", sessionChecker, (req, res) => {
+//   // res.json('Hello World');
+//   if (req.session.user) {
+//     console.log("today: ", req.session.user);
+//     res.status(200).json({ username: req.session.user });
+//   } else {
+//     // res.clearCookie('user_sid');
+//     // next();
+//     res.status(400).json("No Valid Login");
+//   }
+// });
 
-    // res.status(200).json({username: req.session.user});
-    // if (req.session.views) {
-    //     req.session.views++;
-    //   }
-    //   else {
-    //     req.session.views = 1;
-    //   }
-    //   res.send(`${req.session.views} views`);
+app.get("/auth", (req, res) => {
+  if (req.session.user) {
+    // console.log("today: ", req.session.user);
+    res.status(200).json({ username: req.session.user });
+  } else {
+    // res.clearCookie('user_sid');
+    // next();
+    res.status(400).json("No Valid Session");
+  }
 });
 
-app.get('/maxPostCount', Controller.getMaxPostCount(db));
+app.get("/maxPostCount", Controller.getMaxPostCount(db));
 
 // app.put('/register', (req, res) => Controller.registerUser(db, req, res, bcrypt, saltRounds));
-app.put('/register', Controller.registerUser(db, bcrypt, saltRounds));
+app.put("/register", Controller.registerUser(db, bcrypt, saltRounds));
 
-app.post('/signin', Controller.signInUser(db, bcrypt));
+app.post("/signin", Controller.signInUser(db, bcrypt));
 
-app.get('/getPosts', Controller.getPosts(db));
-app.get('/getPosts/:page', sessionChecker, Controller.getPosts(db));
+app.get("/getPosts", Controller.getPosts(db));
+app.get("/getPosts/:page", sessionChecker, Controller.getPosts(db));
 
-app.put('/submitPost', Controller.submitPost(db));
+app.put("/submitPost", Controller.submitPost(db));
 
-app.get('/logout', (req, res, next) => {
-    // res.clearCookie('user_sid', {path: '/'});
-    // console.log('logout user: ', req.session.user);
-    res.clearCookie('user_sid');
-    // console.log('2logout user: ', req.sessionID);
-    if (req.session.user) {
-        // res.clearCookie('user_sid', {domain: '127.0.0.1', path: '/'});
-        req.session.destroy(function(err){
-            if(err){
-               console.log(err);
-            }else{
-                res.status(200).json({status:'Logged Out'});
-                // res.clearCookie('user_sid', {domain: 'localhost:3000', path: '/'});
-            }
-         });
-        // console.log('destroy: ', req.session);
-        // res.redirect('/');
-    } else {
-        // res.redirect('/login');
-    }
+app.get("/logout", (req, res, next) => {
+  // res.clearCookie('user_sid', {path: '/'});
+  // console.log('logout user: ', req.session.user);
+  res.clearCookie("user_sid");
+  // console.log('2logout user: ', req.sessionID);
+  if (req.session.user) {
+    // res.clearCookie('user_sid', {domain: '127.0.0.1', path: '/'});
+    req.session.destroy(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.status(200).json({ status: "Logged Out" });
+        // res.clearCookie('user_sid', {domain: 'localhost:3000', path: '/'});
+      }
+    });
+    // console.log('destroy: ', req.session);
+    // res.redirect('/');
+  } else {
+    // res.redirect('/login');
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server started on Port ${PORT}.`);
+  console.log(`Server started on Port ${PORT}.`);
 });
